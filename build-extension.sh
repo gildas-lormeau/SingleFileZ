12447e1a5684a4014a6651baf748d312a22d313d
Gildas <gildas.lormeau@gmail.com>
2019-05-17 01:12:25 +0200

    updated script for chromium

--- build-extension.sh
+++ build-extension.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-zip -r singlefilez-extension.zip manifest.json extension lib _locales index.js
+rm singlefilez-extension-firefox.zip singlefilez-extension-chromium.zip
+zip -r singlefilez-extension-firefox.zip manifest.json extension lib _locales index.js
+cp manifest.json manifest.copy.json
+jq "del(.applications,.permissions[0],.options_ui.browser_style)" manifest.copy.json > manifest.json
+zip -r singlefilez-extension-chromium.zip manifest.json extension lib _locales index.js
+mv manifest.copy.json manifest.json
