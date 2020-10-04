bc38aea5ee3666a3bc7934f2207c85e43d1e6e79
Gildas <gildas.lormeau@gmail.com>
2020-10-04 23:57:12 +0200

    removed obsolete code

--- extension/ui/content/content-ui-main.js
+++ extension/ui/content/content-ui-main.js
@@ -69,7 +69,6 @@ this.singlefile.extension.ui.content.main = this.singlefile.extension.ui.content
 					maskElement.offsetWidth;
 					maskElement.style.setProperty("background-color", "black", "important");
 					maskElement.style.setProperty("opacity", .3, "important");
-					document.body.offsetWidth;
 				}
 			}
 		},
