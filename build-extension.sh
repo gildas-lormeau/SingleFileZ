b6fab067bb86fc93ede7a54d43179a9413463c27
Gildas <gildas.lormeau@gmail.com>
2020-12-03 13:15:12 +0100

    add support of Edge

--- build-extension.sh
+++ build-extension.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm singlefilez-extension-firefox.zip singlefilez-extension-chromium.zip
+rm singlefilez-extension-firefox.zip singlefilez-extension-chromium.zip singlefilez-extension-edge.zip
 cp manifest.json manifest.copy.json
 cp extension/core/bg/downloads.js downloads.copy.js
 sed -i 's/7544745492-ig6uqhua0ads4jei52lervm1pqsi6hot/7544745492-oe3q2jjvdluks2st2smslmrofcdederh/g' extension/core/bg/downloads.js
@@ -11,9 +11,23 @@ sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' extension/core/bg/co
 zip -r singlefilez-extension-firefox.zip manifest.json common extension lib _locales
 mv config.copy.js extension/core/bg/config.js
 
-jq "del(.applications,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
 sed -i 's/7544745492-ig6uqhua0ads4jei52lervm1pqsi6hot/7544745492-oe3q2jjvdluks2st2smslmrofcdederh/g' manifest.json
 zip -r singlefilez-extension-chromium.zip manifest.json common extension lib _locales
+
+cp extension/core/bg/config.js config.copy.js
+jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json > manifest.json
+sed -i 's/7544745492-ig6uqhua0ads4jei52lervm1pqsi6hot/7544745492-oe3q2jjvdluks2st2smslmrofcdederh/g' manifest.json
+sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' extension/core/bg/config.js
+mkdir _locales.copy
+cp -R _locales/* _locales.copy
+rm -rf _locales/*
+cp -R _locales.copy/en _locales
+zip -r singlefilez-extension-edge.zip manifest.json common extension lib _locales
+rm -rf _locales/*
+mv _locales.copy/* _locales
+rmdir _locales.copy
+mv config.copy.js extension/core/bg/config.js
 
 mv manifest.copy.json manifest.json
 mv downloads.copy.js extension/core/bg/downloads.js
