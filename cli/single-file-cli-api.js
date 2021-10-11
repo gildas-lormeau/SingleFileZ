051f2b4de127295734a6d88be3f33fd26aeaa783
Gildas <gildas.lormeau@gmail.com>
2021-10-12 00:25:18 +0200

    ignore all the paths but not the last (see #108)

--- cli/single-file-cli-api.js
+++ cli/single-file-cli-api.js
@@ -257,7 +257,7 @@ function getFilename(filename, options, index = 1) {
 		if (outputDirectory.startsWith("/")) {
 			options.outputDirectory = outputDirectory;
 		} else {
-			options.outputDirectory = options.outputDirectory.join("");
+			options.outputDirectory = options.outputDirectory[0] + outputDirectory;
 		}
 	}
 	let outputDirectory = options.outputDirectory || "";
