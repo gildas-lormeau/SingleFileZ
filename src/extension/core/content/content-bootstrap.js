528dccdb8332a2084e7646b2c07efc0f14a2b09a
Gildas <gildas.lormeau@gmail.com>
2022-04-10 04:19:44 +0200

    fix bootstrap

--- src/extension/core/content/content-bootstrap.js
+++ src/extension/core/content/content-bootstrap.js
@@ -25,6 +25,8 @@
 
 const singlefile = globalThis.singlefileBootstrap;
 
+const MAX_CONTENT_SIZE = 32 * (1024 * 1024);
+
 let unloadListenerAdded, options, tabId, tabIndex, autoSaveEnabled, autoSaveTimeout, autoSavingPage, pageAutoSaved, previousLocationHref;
 singlefile.pageInfo = {
 	updatedResources: {},
@@ -60,6 +62,24 @@ if (globalThis.window == globalThis.top && location && location.href && (locatio
 
 async function extractFile() {
 	if (document.documentElement.dataset.sfz !== undefined) {
+		const data = await getContent();
+		executeBootstrap(data);
+	} else {
+		if ((document.body && document.body.childNodes.length == 1 && document.body.childNodes[0].tagName == "PRE" && /<html[^>]* data-sfz[^>]*>/i.test(document.body.childNodes[0].textContent))) {
+			const doc = (new DOMParser()).parseFromString(document.body.childNodes[0].textContent, "text/html");
+			document.replaceChild(doc.documentElement, document.documentElement);
+			document.querySelectorAll("script").forEach(element => {
+				const scriptElement = document.createElement("script");
+				scriptElement.textContent = element.textContent;
+				element.parentElement.replaceChild(scriptElement, element);
+			});
+			await extractFile();
+		}
+	}
+}
+
+function getContent() {
+	return new Promise(resolve => {
 		const xhr = new XMLHttpRequest();
 		xhr.open("GET", location.href);
 		xhr.send();
@@ -92,23 +112,12 @@ async function extractFile() {
 					}
 					if (!message.truncated || message.finished) {
 						browser.runtime.onMessage.removeListener(fetchResponse);
-						executeBootstrap(pendingResponseArray);
+						resolve(pendingResponseArray);
 					}
 				}
 			}
 		};
-	} else {
-		if ((document.body && document.body.childNodes.length == 1 && document.body.childNodes[0].tagName == "PRE" && /<html[^>]* data-sfz[^>]*>/i.test(document.body.childNodes[0].textContent))) {
-			const doc = (new DOMParser()).parseFromString(document.body.childNodes[0].textContent, "text/html");
-			document.replaceChild(doc.documentElement, document.documentElement);
-			document.querySelectorAll("script").forEach(element => {
-				const scriptElement = document.createElement("script");
-				scriptElement.textContent = element.textContent;
-				element.parentElement.replaceChild(scriptElement, element);
-			});
-			await extractFile();
-		}
-	}
+	});
 }
 
 function executeBootstrap(data) {
