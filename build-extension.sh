3747424e4eb4552b8079abc159e70f03f41da63f
Gildas <gildas.lormeau@gmail.com>
2022-04-15 23:21:45 +0200

    fix build and bump version

--- build-extension.sh
+++ build-extension.sh
@@ -13,12 +13,12 @@ cp src/extension/core/bg/config.js config.copy.js
 jq "del(.options_page,.background.persistent,.optional_permissions[0],.oauth2)" manifest.copy.json >manifest.json
 sed -i 's/7544745492-ig6uqhua0ads4jei52lervm1pqsi6hot/7544745492-oe3q2jjvdluks2st2smslmrofcdederh/g' manifest.json
 sed -i 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' src/extension/core/bg/config.js
-zip -r singlefilez-extension-firefox.zip manifest.json lib _locales src/extension src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
+zip -r singlefilez-extension-firefox.zip manifest.json lib _locales src/extension src/single-file/vendor/zip/zip.js src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
 mv config.copy.js src/extension/core/bg/config.js
 
 jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json >manifest.json
 sed -i 's/7544745492-ig6uqhua0ads4jei52lervm1pqsi6hot/7544745492-oe3q2jjvdluks2st2smslmrofcdederh/g' manifest.json
-zip -r singlefilez-extension-chromium.zip manifest.json lib _locales src/extension src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
+zip -r singlefilez-extension-chromium.zip manifest.json lib _locales src/single-file/vendor/zip/zip.js src/extension src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
 
 cp src/extension/core/bg/config.js config.copy.js
 jq "del(.browser_specific_settings,.permissions[0],.permissions[1],.options_ui.browser_style)" manifest.copy.json >manifest.json
@@ -27,7 +27,7 @@ mkdir _locales.copy
 cp -R _locales/* _locales.copy
 rm -rf _locales/*
 cp -R _locales.copy/en _locales
-zip -r singlefilez-extension-edge.zip manifest.json lib _locales src/extension src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
+zip -r singlefilez-extension-edge.zip manifest.json lib _locales src/extension src/single-file/vendor/zip/zip.js src/single-file/vendor/zip/zip.min.js src/single-file/vendor/zip/z-worker.js
 rm -rf _locales/*
 mv _locales.copy/* _locales
 rmdir _locales.copy
