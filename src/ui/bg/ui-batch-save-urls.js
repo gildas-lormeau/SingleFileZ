6541fac5c29272f790ed3a0193989c43fe2dc2a0
Gildas <gildas.lormeau@gmail.com>
2023-12-19 00:51:37 +0100

    remove obsolete code

--- src/ui/bg/ui-batch-save-urls.js
+++ src/ui/bg/ui-batch-save-urls.js
@@ -50,7 +50,7 @@ addUrlsLabel.textContent = browser.i18n.getMessage("pendingsAddUrls");
 URLLabel.textContent = browser.i18n.getMessage("batchSaveUrlsURLTitle");
 addUrlForm.onsubmit = () => {
 	const value = addUrlInput.value.trim();
-	if (value.length && !urls.includes(value) && (value.startsWith("http://") || value.startsWith("https://") || value.startsWith("file://"))) {
+	if (!urls.includes(value)) {
 		urls.push(value);
 		addUrlInput.value = "";
 		refresh();
